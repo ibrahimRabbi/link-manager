ca077389423ffb568f1cfd1eb5b7d6b74028cbc4
unknown <rjmahir.faisal@gmail.com>
2022-11-15 01:34:46 +0600

    display selection dialog conditonally

--- src/Components/NewLink/NewLink.jsx
+++ src/Components/NewLink/NewLink.jsx
@@ -1,11 +1,13 @@
-import { Button, StructuredListBody, StructuredListCell, StructuredListRow, StructuredListWrapper } from '@carbon/react';
-import React, { useEffect } from 'react';
+import { Button, Checkbox, Search, StructuredListBody, StructuredListCell, StructuredListRow, StructuredListWrapper } from '@carbon/react';
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
-import { handleCancelLink, handleCreateLink, handleCurrPageTitle, handleLinkType, handleProjectType, handleResourceType, handleUpdateCreatedLink } from '../../Redux/slices/linksSlice';
+import { handleCancelLink, handleCreateLink, handleCurrPageTitle, handleLinkType, handleProjectType, handleResourceType, handleTargetDataArr, handleUpdateCreatedLink } from '../../Redux/slices/linksSlice';
+import UseDataTable from '../Shared/UseDataTable/UseDataTable';
 import UseDropdown from '../Shared/UseDropdown/UseDropdown';
-import { btnContainer, dropdownStyle, linkTypeContainer, sourceContainer, sourceProp, sourceValue, targetContainer, targetIframe } from './NewLink.module.scss';
+import { btnContainer, dropdownStyle, emptySearchWarning, inputContainer, linkTypeContainer, newLinkTable, searchContainer, searchInput, sourceContainer, sourceProp, sourceValue, targetContainer, targetIframe, targetSearchContainer } from './NewLink.module.scss';
 
 // dropdown items
 const linkTypeItems = ['affectedBy', 'implementedBy', 'trackedBy', 'constrainedBy', 'decomposedBy', 'elaboratedBy', 'satisfiedBy'];
@@ -13,18 +15,19 @@ const projectItems = ['GCM System - Backend (JIRA)', 'GCM UI - Frontend (JIRA)',
 const resourceItems = ['User story', 'Task', 'Epic', 'Bug', 'Improvement'];
 
 // Table header 
-// const headers = [
-//   { key: 'identifier', header: 'Identifier' },
-//   { key: 'name', header: 'Name' },
-//   { key: 'description', header: 'Description' },
-//   { key: 'checkbox', header: <Checkbox labelText='' id='' /> }
-// ];
+const headers = [
+  { key: 'identifier', header: 'Identifier' },
+  { key: 'name', header: 'Name' },
+  { key: 'description', header: 'Description' },
+  { key: 'checkbox', header: <Checkbox labelText='' id='' /> }
+];
 
 const NewLink = ({ pageTitle }) => {
-  const {isWbe, sourceDataList, linkType, projectType, resourceType, editLinkData } = useSelector(state => state.links);
-  // const { register, handleSubmit } = useForm();
-  // const [searchText, setSearchText] = useState(null);
-  // const [displayTableData, setDisplayTableData] = useState([]);
+  const {isWbe, sourceDataList, linkType, projectType, resourceType, editLinkData, targetDataArr, editTargetData } = useSelector(state => state.links);
+  const { register, handleSubmit } = useForm();
+  const [searchText, setSearchText] = useState(null);
+  const [isJiraApp, setIsJiraApp] = useState(false);
+  const [displayTableData, setDisplayTableData] = useState([]);
   const navigate = useNavigate();
   const {pathname}=useLocation();
   const dispatch = useDispatch();
@@ -34,34 +37,39 @@ const NewLink = ({ pageTitle }) => {
   },[]);
 
   useEffect(()=>{
-    pageTitle?null:dispatch(handleCancelLink());
+    pageTitle?null: dispatch(handleCancelLink());
   },[pathname]);
 
+  
+  useEffect(()=>{
+    if(projectType) setIsJiraApp(projectType?.includes('JIRA'));
+  },[projectType]);
+
   // Edit link options start
-  // useEffect(() => {
-  //   if (editTargetData?.identifier) {
-  //     const string = editTargetData?.description?.split(' ')[0]?.toLowerCase();
-  //     setSearchText(string === 'document' ? 'document' : string === 'user' ? 'data' : null);
-  //   }
-  // }, [pageTitle]);
+  useEffect(() => {
+    if (editTargetData?.identifier) {
+      const string = editTargetData?.description?.split(' ')[0]?.toLowerCase();
+      setSearchText(string === 'document' ? 'document' : string === 'user' ? 'data' : null);
+    }
+  }, [pageTitle]);
   // Edit link options end
 
   // search data or document 
-  // useEffect(() => {
-  //   setDisplayTableData([]);
-  //   const URL = editTargetData?.identifier ? `../../${searchText}.json` : `../../${searchText}.json`;
-  //   if(searchText){
-  //     fetch(URL)
-  //       .then(res => res.json())
-  //       .then(data => setDisplayTableData(data))
-  //       .catch(() => { });
-  //   }
-  // }, [searchText]);
+  useEffect(() => {
+    setDisplayTableData([]);
+    const URL = editTargetData?.identifier ? `../../${searchText}.json` : `../../${searchText}.json`;
+    if(searchText){
+      fetch(URL)
+        .then(res => res.json())
+        .then(data => setDisplayTableData(data))
+        .catch(() => { });
+    }
+  }, [searchText]);
 
-  // const handleSearchData = data => {
-  //   dispatch(handleTargetDataArr(null));
-  //   setSearchText(data?.searchText);
-  // };
+  const handleSearchData = data => {
+    dispatch(handleTargetDataArr(null));
+    setSearchText(data?.searchText);
+  };
 
   // Link type dropdown
   const handleLinkTypeChange = ({ selectedItem }) => {
@@ -84,9 +92,9 @@ const NewLink = ({ pageTitle }) => {
   };
 
   // Selected target data
-  // const handleSelectedData = (data, value) => {
-  //   dispatch(handleTargetDataArr({ data, value }));
-  // };
+  const handleSelectedData = (data, value) => {
+    dispatch(handleTargetDataArr({ data, value }));
+  };
 
   // Edit created link
   const handleLinkUpdate = () => {
@@ -149,50 +157,63 @@ const NewLink = ({ pageTitle }) => {
         <div className={targetContainer}>
           <h5>Target</h5>
 
-          <div className={targetIframe}>
-            <iframe src='https://192.241.220.34:9443/rm/pickers/com.ibm.rdm.web.RRCPicker?projectURL=https://192.241.220.34:9443/rm/rm-projects/_VhNr0IEzEeqnsvH-FkjSvQ#oslc-core-postMessage-1.0' height='550px' width='800px'></iframe>
-            {/*<iframe src='http://localhost:5001/oslc/provider/selector' height='550px' width='800px'></iframe>*/}
-            {/*you will receive the information coming from the Selection Dialog*/}
-          </div>
-
-          {/* <div className={targetSearchContainer}>
-            <form onSubmit={handleSubmit(handleSearchData)} className={searchContainer}>
-              <div className={inputContainer}>
-                <Search
-                  id=''
-                  labelText=''
-                  className={searchInput} 
-                  type='text' 
-                  placeholder='Search by identifier or name' 
-                  {...register('searchText')}
-                  size='md'
-                />
-              </div>
-              <Button kind='primary' size='md' type='submit'>Search</Button>
-            </form>
-          </div>
+          {
+            isJiraApp && <div className={targetIframe}>
+              <iframe src='https://192.241.220.34:9443/rm/pickers/com.ibm.rdm.web.RRCPicker?projectURL=https://192.241.220.34:9443/rm/rm-projects/_VhNr0IEzEeqnsvH-FkjSvQ#oslc-core-postMessage-1.0' height='550px' width='800px'></iframe>
+              {/*<iframe src='http://localhost:5001/oslc/provider/selector' height='550px' width='800px'></iframe>*/}
+              {/*you will receive the information coming from the Selection Dialog*/}
+            </div>
+          }
 
           {
-            (searchText && displayTableData[0] || pageTitle) &&
+            !isJiraApp && 
+            <>
+              <div className={targetSearchContainer}>
+                <form onSubmit={handleSubmit(handleSearchData)} className={searchContainer}>
+                  <div className={inputContainer}>
+                    <Search
+                      id=''
+                      labelText=''
+                      className={searchInput} 
+                      type='text' 
+                      placeholder='Search by identifier or name' 
+                      {...register('searchText')}
+                      size='md'
+                    />
+                  </div>
+                  <Button kind='primary' size='md' type='submit'>Search</Button>
+                </form>
+              </div>
+
+              {
+                (searchText && displayTableData[0] || pageTitle) &&
             <div className={newLinkTable}>
               <UseDataTable headers={headers} tableData={displayTableData} isCheckBox={true} isChecked={editLinkData?.targetData?.identifier} editTargetData={editTargetData} isPagination={displayTableData[0] ? true : false} selectedData={handleSelectedData} />
             </div>
-          }
-          {(searchText && !displayTableData[0]) &&
+              }
+              {(searchText && !displayTableData[0]) &&
             <h2 className={emptySearchWarning}>Please search by valid identifier or name</h2>
-          } */}
+              }
+              
+            </>
+          }
+          
+          { !isJiraApp &&
+            <>
+              {/* new link btn  */}
+              {(projectType && resourceType &&  targetDataArr[0] &&!pageTitle) && <div className={btnContainer}>
+                <Button kind='secondary' onClick={handleCancelOpenedLink} size='md'>Cancel</Button>
+                <Button kind='primary' onClick={handleSaveLink} size='md'>Save</Button>
+              </div>}
 
-          {/* new link btn  */}
-          {(projectType && resourceType && !pageTitle) && <div className={btnContainer}>
-            <Button kind='secondary' onClick={handleCancelOpenedLink} size='md'>Cancel</Button>
-            <Button kind='primary' onClick={handleSaveLink} size='md'>Save</Button>
-          </div>}
+              {/* edit link btn  */}
+              {(pageTitle && editLinkData?.id) && <div className={btnContainer}>
+                <Button kind='secondary' onClick={handleCancelOpenedLink} size='md'>Cancel</Button>
+                <Button kind='primary' onClick={handleLinkUpdate} size='md'>Save</Button>
+              </div>}
+            </>
+          }
 
-          {/* edit link btn  */}
-          {(pageTitle && editLinkData?.id) && <div className={btnContainer}>
-            <Button kind='secondary' onClick={handleCancelOpenedLink} size='md'>Cancel</Button>
-            <Button kind='primary' onClick={handleLinkUpdate} size='md'>Save</Button>
-          </div>}
         </div>
       }
     </div>
