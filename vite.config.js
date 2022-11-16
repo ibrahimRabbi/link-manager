2b466383689b2e69dd2bc171608098ae8e390f31
isccarrasco <mario.carrasco@gmail.com>
2022-11-16 00:01:33 -0600

    Configuring vite to load the fonts from the assets within the node_module
    Signed-off-by: isccarrasco <mario.carrasco@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  includeAssets: ['favicon.svg', 'favicon.ico', 'assets/*'],
+  resolve: {
+    alias: {
+      '~@ibm': path.resolve(__dirname, 'node_modules/@ibm'),
+    }
+  },
 });
