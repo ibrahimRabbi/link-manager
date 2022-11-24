5b59360f493cb13624457e76266af74c692e0919
unknown <rjmahir.faisal@gmail.com>
2022-11-24 11:53:31 +0600

    loading problem fixed

--- src/Components/Shared/ProtectedRoute/ProtectedRoute.jsx
+++ src/Components/Shared/ProtectedRoute/ProtectedRoute.jsx
@@ -5,7 +5,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 const ProtectedRoute = ({ children }) => {
   let { loggedInUser, isLoading } = useSelector(state=>state.links);
   let location = useLocation();
-  if(isLoading || !loggedInUser?.userName) return <h1 className='text-center'>Loading...</h1>;
+  if(isLoading) return <h1 className='text-center'>Loading...</h1>;
 
   if (loggedInUser?.userName) {
     return children;
