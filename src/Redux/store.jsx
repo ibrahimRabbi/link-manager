31c87b17a0c4dfa7210718835ae1d45eb5fdb995
isccarrasco <mario.carrasco@gmail.com>
2023-03-22 20:52:56 -0600

    Adding sentry to the UI application
    Signed-off-by: isccarrasco <mario.carrasco@gmail.com>

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -5,6 +5,26 @@ import navReducer from './slices/navSlice';
 import usersReducer from './slices/usersSlice';
 import organizationsReducer from './slices/organizationSlice';
 import applicationsReducer from './slices/applicationSlice';
+import * as Sentry from '@sentry/react';
+
+const sentryReduxEnhancer = Sentry.createReduxEnhancer({
+  // Optionally pass options listed below
+  actionTransformer: (action) => {
+    if (action.type === 'GOVERNMENT_SECRETS') {
+      // Return null to not log the action to Sentry
+      return null;
+    }
+    if (action.type === 'SET_PASSWORD') {
+      // Return a transformed action to remove sensitive information
+      return {
+        ...action,
+        password: null,
+      };
+    }
+
+    return action;
+  },
+});
 
 const store = configureStore({
   reducer: {
@@ -15,5 +35,6 @@ const store = configureStore({
     organizations: organizationsReducer,
     applications: applicationsReducer,
   },
+  sentryReduxEnhancer,
 });
 export default store;
--- src/index.jsx
+++ src/index.jsx
@@ -8,6 +8,21 @@ import './index.scss';
 import store from './Redux/store.jsx';
 import reportWebVitals from './reportWebVitals';
 import { AuthContextProvider } from './Store/Auth-Context.jsx';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/react';
+
+const SENTRY_DSN = `${process.env.REACT_APP_SENTRY_DSN}`;
+
+Sentry.init({
+  // eslint-disable-next-line max-len
+  dsn: SENTRY_DSN,
+  integrations: [new BrowserTracing()],
+
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  normalizeDepth: 10,
+});
 
 ReactDOM.render(
   <React.StrictMode>
