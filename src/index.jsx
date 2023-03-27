63074d925b1f2596f9f3be08ea814e00a7670157
isccarrasco <mario.carrasco@gmail.com>
2023-03-27 17:07:37 -0600

    Adding replay feature of sentry
    Signed-off-by: isccarrasco <mario.carrasco@gmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -16,12 +16,19 @@ const SENTRY_DSN = `${process.env.REACT_APP_SENTRY_DSN}`;
 Sentry.init({
   // eslint-disable-next-line max-len
   dsn: SENTRY_DSN,
-  integrations: [new BrowserTracing()],
+  integrations: [new BrowserTracing(), new Sentry.Replay()],
 
   // We recommend adjusting this value in production, or using tracesSampler
   // for finer control
   tracesSampleRate: 1.0,
   normalizeDepth: 10,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
 });
 
 ReactDOM.render(
