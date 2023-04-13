838b33e88b2dbcbe5ad1b1d0b9e32ff58fd2f3e9
Faisal <mahir.faisal@koneksys.com>
2023-04-14 02:56:37 +0600

    inputs and form added from rsuite

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,5 @@
 import React, { useContext, useState } from 'react';
-
-import { ArrowRight } from '@carbon/icons-react';
-import { Button, PasswordInput, ProgressBar, TextInput } from '@carbon/react';
+// import {PasswordInput, ProgressBar, TextInput } from '@carbon/react';
 import { useForm } from 'react-hook-form';
 import { useLocation, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
@@ -9,15 +7,22 @@ import AuthContext from '../../Store/Auth-Context.jsx';
 import style from './Login.module.scss';
 import { useSelector } from 'react-redux';
 import { useMixpanel } from 'react-mixpanel-browser';
+import { ButtonToolbar, FlexboxGrid, Button, Form, Panel } from 'rsuite';
 
-const { main, container, title, formContainer, btnContainer, titleSpan, errText } = style;
+const {
+  // formContainer, btnContainer,
+  titleSpan,
+  main,
+  title,
+  errText,
+} = style;
 const loginURL = `${process.env.REACT_APP_LM_REST_API_URL}/auth/login`;
 
 // const mixpanelToken= process.env.REACT_APP_MIXPANEL_TOKEN;
-
 const Login = () => {
   const { isWbe } = useSelector((state) => state.links);
   const [isLoading, setIsLoading] = useState(false);
+
   const authCtx = useContext(AuthContext);
   const location = useLocation();
   const navigate = useNavigate();
@@ -82,43 +87,85 @@ const Login = () => {
   };
 
   return (
-    <div className={main}>
-      <div className={container}>
-        <h3 className={title}>
-          Link Manager Application <br />
-          <span className={titleSpan}>Please Login</span>
-        </h3>
+    <>
+      <div className={main}>
+        {/* <form onSubmit={handleSubmit(onSubmit)} className={formContainer}>
+            <TextInput
+              type="text"
+              id="userName"
+              labelText="User name"
+              placeholder="Enter user name"
+              {...register('userName', { required: true })}
+            />
+            <p className={errText}>{errors.userName && 'Invalid User'}</p>
 
-        <form onSubmit={handleSubmit(onSubmit)} className={formContainer}>
-          <TextInput
-            type="text"
-            id="userName"
-            labelText="User name"
-            placeholder="Enter user name"
-            {...register('userName', { required: true })}
-          />
-          <p className={errText}>{errors.userName && 'Invalid User'}</p>
-
-          <PasswordInput
-            type="password"
-            id="login_password_id"
-            labelText="Password"
-            placeholder="Enter your password"
-            autoComplete="on"
-            {...register('password', { required: true, minLength: 5 })}
-          />
-          <p className={errText}>
-            {errors.password && 'Password should include at least 5 characters'}
-          </p>
-          {isLoading && <ProgressBar label="" />}
-          <div className={btnContainer}>
-            <Button renderIcon={ArrowRight} size="lg" kind="primary" type="submit">
+            <PasswordInput
+              type="password"
+              id="login_password_id"
+              labelText="Password"
+              placeholder="Enter your password"
+              autoComplete="on"
+              {...register('password', { required: true, minLength: 5 })}
+            />
+            <p className={errText}>
+              {errors.password && 'Password should include at least 5 characters'}
+            </p>
+            {isLoading && <ProgressBar label="" />}
+            <div className={btnContainer}>
+              <Button color='blue'
+                appearance="primary" type="submit">
               Sign in
-            </Button>
-          </div>
-        </form>
+              </Button>
+            </div>
+          </form> */}
+
+        {isLoading && <h5 style={{ textAlign: 'center' }}>Loading...</h5>}
+        <FlexboxGrid justify="center" align="middle">
+          <FlexboxGrid.Item colspan={12}>
+            <Panel
+              header={
+                <h3 className={title}>
+                  Link Manager Application <br />
+                  <span className={titleSpan}>Please Login</span>
+                </h3>
+              }
+              bordered
+            >
+              <Form fluid onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group>
+                  <Form.Control
+                    name="username"
+                    placeholder="User Name"
+                    {...register('userName', { required: true })}
+                  />
+                  <p className={errText}>{errors.userName && 'Invalid User Name'}</p>
+                </Form.Group>
+
+                <Form.Group>
+                  <Form.Control
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    {...register('password', { required: true, minLength: 5 })}
+                  />
+                  <p className={errText}>
+                    {errors.password && 'Password should include at least 5 characters'}
+                  </p>
+                </Form.Group>
+
+                <Form.Group>
+                  <ButtonToolbar>
+                    <Button color="blue" block appearance="primary" type="submit">
+                      Sign in
+                    </Button>
+                  </ButtonToolbar>
+                </Form.Group>
+              </Form>
+            </Panel>
+          </FlexboxGrid.Item>
+        </FlexboxGrid>
       </div>
-    </div>
+    </>
   );
 };
 
