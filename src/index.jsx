f1da39f6f3e8b613f361ed51a8959c68543e2206
Juan Quintanar <juan.quintanar@koneksys.com>
2023-05-23 22:52:01 -0600

    Displaying only outgoing nodes in UI

--- src/index.jsx
+++ src/index.jsx
@@ -9,30 +9,30 @@ import './index.less';
 import store from './Redux/store.jsx';
 import reportWebVitals from './reportWebVitals';
 import { AuthContextProvider } from './Store/Auth-Context.jsx';
-import * as Sentry from '@sentry/react';
-import { BrowserTracing, Replay } from '@sentry/react';
-
-const SENTRY_DSN = `${process.env.REACT_APP_SENTRY_DSN}`;
-const ENVIRONMENT = `${process.env.NODE_ENV}`;
-
-Sentry.init({
-  // eslint-disable-next-line max-len
-  dsn: SENTRY_DSN,
-  integrations: [new BrowserTracing(), new Replay()],
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-  normalizeDepth: 10,
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 1.0,
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
-  replaysOnErrorSampleRate: 1.0,
-  environment: ENVIRONMENT,
-});
+// import * as Sentry from '@sentry/react';
+// import { BrowserTracing, Replay } from '@sentry/react';
+//
+// const SENTRY_DSN = `${process.env.REACT_APP_SENTRY_DSN}`;
+// const ENVIRONMENT = `${process.env.NODE_ENV}`;
+//
+// Sentry.init({
+//   // eslint-disable-next-line max-len
+//   dsn: SENTRY_DSN,
+//   integrations: [new BrowserTracing(), new Replay()],
+//
+//   // We recommend adjusting this value in production, or using tracesSampler
+//   // for finer control
+//   tracesSampleRate: 1.0,
+//   normalizeDepth: 10,
+//
+//   // This sets the sample rate to be 10%. You may want this to be 100% while
+//   // in development and sample at a lower rate in production
+//   replaysSessionSampleRate: 1.0,
+//   // If the entire session is not sampled, use the below sample rate to sample
+//   // sessions when an error occurs.
+//   replaysOnErrorSampleRate: 1.0,
+//   environment: ENVIRONMENT,
+// });
 
 ReactDOM.render(
   <React.StrictMode>
