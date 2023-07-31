970cb1df24c58d7892c84aa342ace6dfa0d69e5b
Juan Quintanar <juan.quintanar@koneksys.com>
2023-07-31 11:17:39 -0600

    Add conditional for 304 status

--- src/Redux/apiRequests/API.jsx
+++ src/Redux/apiRequests/API.jsx
@@ -82,6 +82,9 @@ export async function saveResource({ url, token, bodyData, showNotification }) {
         showNotification('success', data.message);
         return data;
       });
+    } else if (res.status === 304) {
+      showNotification('success', 'Resource already linked');
+      return { message: 'Resource already linked' };
     } else {
       if (res.status === 401) {
         return res.json().then((data) => {
