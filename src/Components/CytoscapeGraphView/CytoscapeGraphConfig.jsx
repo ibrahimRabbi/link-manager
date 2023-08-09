75afd110aefd52ea3e35d6d00da04126b8f46d0a
Juan Quintanar <juan.quintanar@koneksys.com>
2023-08-09 16:22:36 -0600

    Add concentric layout

--- src/Components/CytoscapeGraphView/CytoscapeGraphConfig.jsx
+++ src/Components/CytoscapeGraphView/CytoscapeGraphConfig.jsx
@@ -1,5 +1,7 @@
 export const graphLayout = {
-  name: 'random',
+  name: 'concentric',
+  spacingFactor: 2, // Adjust this value to control node separation
+
 };
 
 export const graphStyle = [
@@ -18,6 +20,7 @@ export const graphStyle = [
       width: 3,
       'curve-style': 'straight',
       'target-arrow-shape': 'triangle',
+      'edge-text-rotation': 'autorotate',
     },
   },
   {
@@ -25,12 +28,57 @@ export const graphStyle = [
     style: {
       label: 'data(label)',
       width: 4,
+      'text-background-color': '#ffffff', // Set the background color
+      'text-background-opacity': 1,       // Make the background opaque
+      'text-background-padding': '10px',
     },
   },
   {
-    selector: '.autorotate',
-    style: {
-      'edge-text-rotation': 'autorotate',
+    'selector': 'edge.bezier',
+    'style': {
+      'curve-style': 'bezier',
+      'control-point-step-size': 40,
+    },
+  }, {
+    'selector': 'edge.unbundled-bezier',
+    'style': {
+      'curve-style': 'unbundled-bezier',
+      'control-point-distances': 40,
+      'control-point-weights': 0.5,
+    },
+  }, {
+    'selector': 'edge.multi-unbundled-bezier',
+    'style': {
+      'curve-style': 'unbundled-bezier',
+      'control-point-distances': [40, -40],
+      'control-point-weights': [0.250, 0.75],
+    },
+  }, {
+    'selector': 'edge.haystack',
+    'style': {
+      'curve-style': 'haystack',
+      'haystack-radius': 0.5,
+    },
+  }, {
+    'selector': 'edge.segments',
+    'style': {
+      'curve-style': 'segments',
+      'segment-distances': [ 40, -40 ],
+      'segment-weights': [0.250 , 0.75],
+    },
+  }, {
+    'selector': 'edge.taxi',
+    'style': {
+      'curve-style': 'taxi',
+      'taxi-direction': 'downward',
+      'taxi-turn': 20,
+      'taxi-turn-min-distance': 5,
+    },
+  }, {
+    'selector': 'edge.straight-triangle',
+    'style': {
+      'curve-style': 'straight-triangle',
+      'width': 10,
     },
   },
 ];
