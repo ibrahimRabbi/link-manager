8c7059b61b0c96a4611b0ff90ea81b74946431ab
smfahim25 <smfahimhossen@gmail.com>
2023-09-01 07:55:42 +0600

    added recent project section for landing page

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { useState } from 'react';
+import AuthContext from '../../Store/Auth-Context';
+import { useContext } from 'react';
+import { Message, toaster } from 'rsuite';
+import fetchAPIRequest from '../../apiRequests/apiRequest';
+import { useQuery } from '@tanstack/react-query';
+import RecentProjects from './RecentProjects';
+import UseLoader from '../Shared/UseLoader';
+
+const Home = () => {
+  const [currPage] = useState(1);
+  const [pageSize] = useState(5);
+  const authCtx = useContext(AuthContext);
+  const showNotification = (type, message) => {
+    if (type && message) {
+      const messages = (
+        <Message closable showIcon type={type}>
+          {message}
+        </Message>
+      );
+      toaster.push(messages, { placement: 'bottomCenter', duration: 5000 });
+    }
+  };
+  // get data using react-query
+  const {
+    data: recentProject,
+    isLoading: projectLoading,
+    // refetch: refetchProjects,
+  } = useQuery(['recentProject'], () =>
+    fetchAPIRequest({
+      urlPath: `project/recent?page=${currPage}&per_page=${pageSize}`,
+      token: authCtx.token,
+      method: 'GET',
+      showNotification: showNotification,
+    }),
+  );
+  const {
+    data: recentPipeline,
+    isLoading: pipelineLoading,
+    // refetch: refetchPipeline,
+  } = useQuery(['recentPipeline'], () =>
+    fetchAPIRequest({
+      urlPath: `pipeline_run/recent?page=${currPage}&per_page=${pageSize}`,
+      token: authCtx.token,
+      method: 'GET',
+      showNotification: showNotification,
+    }),
+  );
+
+  console.log(recentPipeline);
+  return (
+    <div style={{ padding: '20px 20px 0 30px' }}>
+      {projectLoading || pipelineLoading ? (
+        <UseLoader />
+      ) : (
+        <div>
+          <h3>Recent Projects</h3>
+          {recentProject?.items?.length < 1 ? (
+            <div>
+              <h3 style={{ textAlign: 'center', marginTop: '10px', color: 'blue' }}>
+                There is no recent projects
+              </h3>
+            </div>
+          ) : (
+            <div>
+              <RecentProjects recentProject={recentProject} />
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/Components/Home/RecentProjects.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Panel } from 'rsuite';
+
+const RecentProjects = ({ recentProject }) => {
+  console.log(recentProject);
+  const Card = (props) => (
+    <Panel {...props} bordered shaded>
+      <div style={{ margin: '10px 0 10px 0' }}>
+        <h6>{props?.project?.name}</h6>
+        <p style={{ marginTop: '2px' }}>{props?.project?.organization?.name}</p>
+      </div>
+    </Panel>
+  );
+  return (
+    <div>
+      <div
+        style={{ display: 'grid', gridTemplateColumns: 'repeat(5,1fr)', gridGap: '10px' }}
+      >
+        {recentProject?.items?.map((project) => (
+          <Card key={project?.id} project={project} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecentProjects;
