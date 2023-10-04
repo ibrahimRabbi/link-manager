5b8dcc623528fe1ec7e62fa6c6023e094c3666b5
ShivamMalan <shivam.malan@koneksys.com>
2023-10-04 22:36:22 +0530

    Cypress E2E Testing with admin Dashbaord

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,18 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:5173',
+    env: {
+      apiUrl:'https://lm-dev.koneksys.com'
+    },
     setupNodeEvents() {
       // implement node event listeners here
     },
   },
   defaultCommandTimeout: 8000, 
   pageLoadTimeout:10000,
+  experimentalSessionAndOrigin: true,
+  chromeWebSecurity: false,
+  screenshotOnRunFailure: false,
 
   component: {
     devServer: {
