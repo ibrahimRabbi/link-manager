ea9dd29c008cc2b847db7f7e53e0f10c8421a038
smfahim25 <smfahimhossen@gmail.com>
2023-10-05 16:56:25 +0600

    added directional icon

--- src/Components/AdminDasComponents/MigrationConfig/MigrationConfig.jsx
+++ src/Components/AdminDasComponents/MigrationConfig/MigrationConfig.jsx
@@ -27,10 +27,15 @@ import AuthContext from '../../../Store/Auth-Context';
 import UseIconSelect from '../../SelectionDialog/GlobalSelector/UseIconSelect';
 import AppIconSelect from './AppIconSelect';
 import UseLoader from '../../Shared/UseLoader';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLeftRight, faRightLong } from '@fortawesome/free-solid-svg-icons';
 
 const apiURL = import.meta.env.VITE_LM_REST_API_URL;
 const thirdApiURL = `${apiURL}/third_party`;
-const direction = [{ name: '---->' }, { name: '<---->' }];
+const direction = [
+  { name: <FontAwesomeIcon icon={faRightLong} style={{ fontSize: '30px' }} /> },
+  { name: <FontAwesomeIcon icon={faLeftRight} style={{ fontSize: '30px' }} /> },
+];
 const MigrationConfig = () => {
   const authCtx = useContext(AuthContext);
   const [externalProjectUrl, setExternalProjectUrl] = useState('');
