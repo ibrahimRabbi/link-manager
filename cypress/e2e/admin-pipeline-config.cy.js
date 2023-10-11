5285130333a1414e97fc55bcfc63608c0117872d
ShivamMalan <shivam.malan@koneksys.com>
2023-10-11 21:43:38 +0530

    Cypress End to End Testing on WBE and new UI upgrades

--- cypress/e2e/admin-pipeline-config.cy.js
+++ cypress/e2e/admin-pipeline-config.cy.js
@@ -2,11 +2,11 @@ describe('The Admin Dashboard test', () => {
   beforeEach(()=>{
       cy.login('mario','admin')
   })
-  it('Admin Dashboard creds',()=>{
+  it('Admin Dashboard Pipeline Config',()=>{
     cy.get('[data-cy="profile-options-btn"]').click();
     cy.get('[data-cy="profile-btns"]').contains("Admin Dashboard").click();
-    cy.get('.admin-side-nav-body').find('ul a.rs-sidenav-item').eq(3).click();
-    cy.get('h3').should('contain', 'Link Types');
+    cy.get('.admin-side-nav-body').find('ul a.rs-sidenav-item').eq(6).click();
+    cy.get('h3').should('contain', 'Pipeline Configuration');
     cy.get('button').contains("Add New").click();
     cy.get('button').contains("Cancel").click();
   })
