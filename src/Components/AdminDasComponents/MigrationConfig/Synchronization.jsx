25dc1a46f16da61c20b80872dcc49f222120e6ae
fahimhossen <fahim.hossen@koneksys.com>
2023-11-16 21:52:36 +0600

    solved the issue

--- src/Components/AdminDasComponents/MigrationConfig/Synchronization.jsx
+++ src/Components/AdminDasComponents/MigrationConfig/Synchronization.jsx
@@ -61,6 +61,9 @@ const Synchronization = () => {
   const authCtx = useContext(AuthContext);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const organization = authCtx?.organization_name
+    ? `/${authCtx?.organization_name?.toLowerCase()}`
+    : '';
 
   // Pagination
   const handlePagination = (value) => {
@@ -137,7 +140,7 @@ const Synchronization = () => {
 
   // handle open add pipeline secret modal
   const handleAddNew = () => {
-    navigate('/admin/createsync');
+    navigate(`${organization}/admin/createsync`);
   };
   const handleDelete = (data) => {
     setDeleteData(data);
