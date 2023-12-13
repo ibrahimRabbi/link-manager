f28f0b7f0f6d7ad1ecea77f1c665ba6579b9f122
Juan Quintanar <juan.quintanar@koneksys.com>
2023-12-13 15:57:13 -0600

    Moving function of application icons to a single module

--- src/Components/AdminDasComponents/Application/Application.jsx
+++ src/Components/AdminDasComponents/Application/Application.jsx
@@ -28,6 +28,7 @@ import Oauth2Waiting from '../ExternalAppIntegrations/Oauth2Waiting/Oauth2Waitin
 import ExternalLogin from '../ExternalAppIntegrations/ExternalLogin/ExternalLogin.jsx';
 import CustomReactSelect from '../../Shared/Dropdowns/CustomReactSelect';
 import AlertModal from '../../Shared/AlertModal';
+import { getApplicationIcons } from '../Icons/application/icons.jsx';
 
 const { modalBodyStyle, step1Container, step2Container, skipBtn } = styles;
 
@@ -544,79 +545,11 @@ const Application = () => {
     (async () => {
       if (allApplications?.items) {
         setLoading(true);
-        // merge icons data with application data
-        const customAppItems = allApplications?.items?.reduce(
-          (accumulator, currentValue) => {
-            // prettier-ignore
-            switch (currentValue?.type) {
-            case 'gitlab':
-              accumulator.push({
-                ...currentValue,
-                iconUrl: '/gitlab_logo.png',
-                status: currentValue?.status,
-              });
-              break;
-            case 'bitbucket':
-              accumulator.push({
-                ...currentValue,
-                iconUrl: '/bitbucket_logo.png',
-                status: currentValue?.status,
-              });
-              break;
-            case 'github':
-              accumulator.push({
-                ...currentValue,
-                iconUrl: '/github_logo.png',
-                status: currentValue?.status,
-              });
-              break;
-            case 'jira':
-              accumulator.push({
-                ...currentValue,
-                iconUrl: '/jira_logo.png',
-                status: currentValue?.status,
-              });
-              break;
-            case 'glideyoke':
-              accumulator.push({
-                ...currentValue,
-                iconUrl: '/glide_logo.png',
-                status: currentValue?.status,
-              });
-              break;
-            case 'valispace':
-              accumulator.push({
-                ...currentValue,
-                iconUrl: '/valispace_logo.png',
-                status: currentValue?.status,
-              });
-              break;
-            case 'codebeamer':
-              accumulator.push({
-                ...currentValue,
-                iconUrl: '/codebeamer_logo.png',
-                status: currentValue?.status,
-              });
-              break;
-            case 'dng':
-              accumulator.push({
-                ...currentValue,
-                iconUrl: '/dng_logo.png',
-                status: currentValue?.status,
-              });
-              break;
-            default:
-              accumulator.push({
-                ...currentValue,
-                iconUrl: '/default_logo.png',
-                status: currentValue?.status,
-              });
-              break;
-            }
-            return accumulator;
-          },
-          [],
-        );
+        // merge icon URLs with application data
+        let customAppItems = [];
+        if (allApplications?.items) {
+          customAppItems = getApplicationIcons(allApplications?.items);
+        }
         setAppsWithIcon(customAppItems);
       }
     })();
