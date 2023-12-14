57478db3b37d95d8f0fffb4059380425ad0933e1
fahimhossen <fahim.hossen@koneksys.com>
2023-12-14 11:32:44 +0600

    hide add new button from pipeline run page

--- src/main.jsx
+++ src/main.jsx
@@ -14,29 +14,29 @@ import process from 'process/browser';
 window.process = process;
 window.global = window;
 
-// // Sentry error tracing setup
-// import * as Sentry from '@sentry/react';
-// import { BrowserTracing, Replay } from '@sentry/react';
+// Sentry error tracing setup
+import * as Sentry from '@sentry/react';
+import { BrowserTracing, Replay } from '@sentry/react';
 
-// const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
-// const ENVIRONMENT = import.meta.env.VITE_NODE_ENV;
+const SENTRY_DSN = import.meta.env.VITE_SENTRY_DSN;
+const ENVIRONMENT = import.meta.env.VITE_NODE_ENV;
 
-// Sentry.init({
-//   dsn: SENTRY_DSN,
-//   integrations: [new BrowserTracing(), new Replay()],
-//   // We recommend adjusting this value in production, or using tracesSampler
-//   // for finer control
-//   tracesSampleRate: 1.0,
-//   normalizeDepth: 10,
+Sentry.init({
+  dsn: SENTRY_DSN,
+  integrations: [new BrowserTracing(), new Replay()],
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  normalizeDepth: 10,
 
-//   // This sets the sample rate to be 10%. You may want this to be 100% while
-//   // in development and sample at a lower rate in production
-//   replaysSessionSampleRate: 1.0,
-//   // If the entire session is not sampled, use the below sample rate to sample
-//   // sessions when an error occurs.
-//   replaysOnErrorSampleRate: 1.0,
-//   environment: ENVIRONMENT,
-// });
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 1.0,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+  environment: ENVIRONMENT,
+});
 
 const queryClient = new QueryClient({
   defaultOptions: {
