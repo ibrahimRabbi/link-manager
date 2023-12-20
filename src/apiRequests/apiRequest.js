e9d9adf192dff09369a575e28f62a63cc8a07762
fahimhossen <fahim.hossen@koneksys.com>
2023-12-20 11:36:57 +0600

    update the code and handled the error

--- src/apiRequests/apiRequest.js
+++ src/apiRequests/apiRequest.js
@@ -17,32 +17,49 @@ export default function fetchAPIRequest({
     body: JSON.stringify(body),
   })
     .then((res) => {
-      if (!res.ok) {
-        throw new Error(`Request failed with status: ${res.status}`);
+      if (res.ok) {
+        if (method === 'GET' && res.status === 204) {
+          showNotification('success', 'No content available');
+          return '';
+        } else if (method === 'DELETE' && res.status === 204) {
+          showNotification('success', 'The content was successfully deleted');
+          return { status: 'success', message: 'The content was successfully deleted' };
+        }
+        return res.json().then((data) => {
+          showNotification('success', data?.message);
+          return data;
+        });
+      } else {
+        res.json().then((data) => {
+          if (res?.status === 404 || res.status === 409) {
+            showNotification('info', data?.message);
+            return false;
+          } else if (res.status === 403) {
+            if (token) {
+              showNotification('error', 'You do not have permission to access');
+              return false;
+            } else {
+              const errorMessage = `${res?.status} not authorized ${data?.message}`;
+              showNotification('error', errorMessage);
+              throw new Error(errorMessage);
+            }
+          }
+          showNotification('error', data?.message);
+          throw new Error(data?.message);
+        });
       }
-      if (method === 'GET' && res.status === 204) {
-        showNotification('success', 'No content available');
-        return '';
-      }
-      if (method === 'DELETE' && res.status === 204) {
-        showNotification('success', 'The content was successfully deleted');
-        return { status: 'success', message: 'The content was successfully deleted' };
-      }
-      return res.json().then((data) => {
-        showNotification('success', data?.message);
-        return data;
-      });
     })
     .catch((error) => {
-      console.error('Fetch error:', error);
-      if (error instanceof TypeError && error.message.includes('Failed to fetch')) {
+      if (error instanceof TypeError && error.message === 'Failed to fetch') {
         showNotification(
           'error',
           'Network error. Please check your internet connection.',
         );
-      } else {
-        showNotification('error', 'An error occurred during the fetch request.');
+        return false;
       }
-      return Promise.reject(error);
+
+      // Handle other errors
+      showNotification('error', error?.message);
+      throw new Error(error?.message);
     });
 }
