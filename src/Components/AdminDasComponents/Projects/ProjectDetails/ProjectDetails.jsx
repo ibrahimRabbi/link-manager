8248015ce792e086dd4ab6b32db679ce692fb177
Juan Quintanar <juan.quintanar@koneksys.com>
2024-01-03 18:10:01 -0600

    Add Loader component in project details

--- src/Components/AdminDasComponents/Projects/ProjectDetails/ProjectDetails.jsx
+++ src/Components/AdminDasComponents/Projects/ProjectDetails/ProjectDetails.jsx
@@ -30,6 +30,7 @@ import { darkBgColor } from '../../../../App.jsx';
 import AdminDataTable from '../../AdminDataTable.jsx';
 import { getApplicationIcons } from '../../Icons/application/icons.jsx';
 import ProjectPipelines from '../ProjectPipelines/ProjectPipelines.jsx';
+import UseLoader from '../../../Shared/UseLoader.jsx';
 
 const lmApiUrl = import.meta.env.VITE_LM_REST_API_URL;
 const { StringType, NumberType, ArrayType } = Schema.Types;
@@ -210,6 +211,7 @@ const ProjectDetails = (props) => {
     // eslint-disable-next-line no-unused-vars
     data: singleProject,
     refetch: refetchSingleProject,
+    isLoading: singleProjectLoading,
   } = useQuery(
     ['project'],
     () => {
@@ -338,210 +340,223 @@ const ProjectDetails = (props) => {
 
   return (
     <FlexboxGrid>
-      {(editData || newProject) && <FlexboxGrid.Item colspan={2}></FlexboxGrid.Item>}
-      <FlexboxGrid.Item
-        colspan={editData || newProject ? 20 : 18}
-        className={mainSection}
-      >
-        {editData && (
-          <p>Update the project details and click save to apply the changes.</p>
-        )}
-        {newProject && (
-          <FlexboxGrid>
-            <FlexboxGrid.Item colspan={2}>
-              <GoProjectTemplate size={100} />
-            </FlexboxGrid.Item>
-            <FlexboxGrid.Item colspan={18}>
-              <p className={newProjectDescription}>
-                Create a blank project to group link nodes, plan your work, among other
-                things.
-              </p>
-            </FlexboxGrid.Item>
-          </FlexboxGrid>
-        )}
-        <FlexboxGrid className={editData ? summarySectionEdit : summarySection}>
-          {editData || newProject ? (
-            <Form
-              fluid
-              style={{ width: '100%' }}
-              ref={projectFormRef}
-              onChange={setFormValue}
-              onCheck={setFormError}
-              formValue={formValue}
-              model={model}
-            >
-              <FlexboxGrid.Item colspan={23} style={{ margin: '25px 0' }}>
-                <TextField name="name" label="Project name" reqText="Required" />
-              </FlexboxGrid.Item>
-
-              <FlexboxGrid.Item colspan={23}>
-                <TextField
-                  name="description"
-                  label="Description"
-                  accepter={TextArea}
-                  rows={3}
-                />
-              </FlexboxGrid.Item>
-
-              <FlexboxGrid.Item colspan={23} style={{ margin: '25px 0' }}>
-                <SelectField
-                  name="applications"
-                  label="Application"
-                  placeholder="Select Application"
-                  accepter={CustomReactSelect}
-                  apiURL={`${lmApiUrl}/${authCtx.organization_id}/application`}
-                  error={formError.applications}
-                  isMulti={true}
-                />
-              </FlexboxGrid.Item>
-              {newProject && (
-                <FlexboxGrid.Item colspan={23}>
-                  <SelectField
-                    name="users"
-                    label="Assign users"
-                    placeholder="Select Users"
-                    accepter={CustomReactSelect}
-                    apiURL={`${lmApiUrl}/user`}
-                    error={formError.users}
-                    isMulti={true}
-                    closeMenuOnSelect={false}
-                  />
-                </FlexboxGrid.Item>
-              )}
-            </Form>
-          ) : (
-            <>
-              <FlexboxGrid.Item colspan={15}>
-                <h3>{projectData?.name ? projectData.name : 'Project info'}</h3>
-              </FlexboxGrid.Item>
-              <FlexboxGrid.Item colspan={5}></FlexboxGrid.Item>
+      {/* eslint-disable-next-line max-len */}
+      {singleProjectLoading ? (
+        <UseLoader />
+      ) : (
+        <>
+          {(editData || newProject) && <FlexboxGrid.Item colspan={2}></FlexboxGrid.Item>}
+          <FlexboxGrid.Item
+            colspan={editData || newProject ? 20 : 18}
+            className={mainSection}
+          >
+            {editData && (
+              <p>Update the project details and click save to apply the changes.</p>
+            )}
+            {newProject && (
               <FlexboxGrid>
-                <FlexboxGrid.Item>
-                  <IconButton
-                    size="md"
-                    title="Invite users"
-                    icon={<FiUsers />}
-                    onClick={() => {
-                      const link = location.pathname;
-                      navigate(`${link}/user-permissions`);
-                    }}
-                    className={resourceButton}
-                  />
+                <FlexboxGrid.Item colspan={2}>
+                  <GoProjectTemplate size={100} />
                 </FlexboxGrid.Item>
-                <FlexboxGrid.Item>
-                  <ProjectOptions
-                    handleEdit={() => setEditData(true)}
-                    handleDelete={() => setOpenDeleteModal(true)}
-                  />
+                <FlexboxGrid.Item colspan={18}>
+                  <p className={newProjectDescription}>
+                    Create a blank project to group link nodes, plan your work, among
+                    other things.
+                  </p>
                 </FlexboxGrid.Item>
               </FlexboxGrid>
-              <FlexboxGrid.Item colspan={23}>
-                <Divider style={{ margin: '5px 0' }} />
-              </FlexboxGrid.Item>
-              <FlexboxGrid.Item colspan={23} className={descriptionSection}>
-                <p>
-                  {projectData?.description ? projectData.description : 'No description'}
-                </p>
-              </FlexboxGrid.Item>
-
-              <FlexboxGrid.Item colspan={23} className={descriptionSection}>
-                <h5>Applications used by the project:</h5>
-                <AdminDataTable props={tableProps} />
-              </FlexboxGrid.Item>
-            </>
-          )}
-        </FlexboxGrid>
-
-        {(editData || newProject) && (
-          <FlexboxGrid className={editButtonSection} colspan={20}>
-            <FlexboxGrid.Item colspan={19}></FlexboxGrid.Item>
-            <FlexboxGrid.Item colspan={2}>
-              <Button
-                appearance="primary"
-                onClick={() => updateProject()}
-                className={editButton}
-              >
-                Save
-              </Button>
-            </FlexboxGrid.Item>
-            {showCancelBtn && (
-              <FlexboxGrid.Item colspan={3}>
-                <Button
-                  appearance="subtle"
-                  onClick={() => resetProjectData()}
-                  className={editButton}
+            )}
+            <FlexboxGrid className={editData ? summarySectionEdit : summarySection}>
+              {editData || newProject ? (
+                <Form
+                  fluid
+                  style={{ width: '100%' }}
+                  ref={projectFormRef}
+                  onChange={setFormValue}
+                  onCheck={setFormError}
+                  formValue={formValue}
+                  model={model}
                 >
-                  Cancel
-                </Button>
-              </FlexboxGrid.Item>
-            )}
-          </FlexboxGrid>
-        )}
-      </FlexboxGrid.Item>
-      {!editData && !newProject && (
-        <FlexboxGrid.Item
-          colspan={6}
-          className={detailsSection}
-          style={{ backgroundColor: isDark === 'dark' && darkBgColor }}
-        >
-          <FlexboxGrid className={detailsTitle} align="middle">
-            <FlexboxGrid.Item colspan={2} className={detailsTitleIcon}>
-              <FaCircleInfo />
-            </FlexboxGrid.Item>
-            <FlexboxGrid.Item colspan={15}>
-              <h3>Project details</h3>
-            </FlexboxGrid.Item>
-          </FlexboxGrid>
-          <Divider style={{ marginTop: '13px' }} />
-          <div
-            className={detailsContent}
-            style={{ backgroundColor: isDark === 'dark' && darkBgColor }}
-          >
-            {projectData?.updated && (
-              <>
-                <p className={detailsSubTitle}>Last updated:</p>
-                <p className={detailsLabel}>{convertTime(projectData.updated)}</p>
-              </>
-            )}
-            {projectData?.created && (
-              <>
-                <p className={detailsSubTitle}>Created:</p>
-                <p className={detailsLabel}>
-                  Created: {convertTime(projectData.created)}
-                </p>
-              </>
-            )}
-            {projectData?.organization?.name && (
-              <>
-                <p className={detailsSubTitle}>Organization:</p>
-                <p className={detailsLabel}>{projectData?.organization?.name}</p>
-              </>
-            )}
-            {authCtx?.user?.role && (
-              <>
-                <p className={detailsSubTitle}>Access level:</p>
-                <p className={detailsLabel}>{authCtx?.user?.role}</p>
-              </>
-            )}
-          </div>
-          <Divider style={{ marginTop: '13px' }} />
-          <FlexboxGrid.Item colspan={15} style={{ marginLeft: '15px' }}>
-            <h5>Latest pipeline runs:</h5>
-          </FlexboxGrid.Item>
-          <FlexboxGrid.Item colspan={24} style={{ marginTop: '-15px' }}>
-            <div style={{ backgroundColor: isDark === 'dark' ? darkBgColor : '#ffffff' }}>
-              <ProjectPipelines />
-            </div>
-          </FlexboxGrid.Item>
-        </FlexboxGrid.Item>
-      )}
+                  <FlexboxGrid.Item colspan={23} style={{ margin: '25px 0' }}>
+                    <TextField name="name" label="Project name" reqText="Required" />
+                  </FlexboxGrid.Item>
 
-      <AlertModal
-        open={openDeleteModal}
-        setOpen={setOpenDeleteModal}
-        content={DELETION_ALERT_MESSAGE}
-        handleConfirmed={handleDeleteConfirmed}
-      />
+                  <FlexboxGrid.Item colspan={23}>
+                    <TextField
+                      name="description"
+                      label="Description"
+                      accepter={TextArea}
+                      rows={3}
+                    />
+                  </FlexboxGrid.Item>
+
+                  <FlexboxGrid.Item colspan={23} style={{ margin: '25px 0' }}>
+                    <SelectField
+                      name="applications"
+                      label="Application"
+                      placeholder="Select Application"
+                      accepter={CustomReactSelect}
+                      apiURL={`${lmApiUrl}/${authCtx.organization_id}/application`}
+                      error={formError.applications}
+                      isMulti={true}
+                    />
+                  </FlexboxGrid.Item>
+                  {newProject && (
+                    <FlexboxGrid.Item colspan={23}>
+                      <SelectField
+                        name="users"
+                        label="Assign users"
+                        placeholder="Select Users"
+                        accepter={CustomReactSelect}
+                        apiURL={`${lmApiUrl}/user`}
+                        error={formError.users}
+                        isMulti={true}
+                        closeMenuOnSelect={false}
+                      />
+                    </FlexboxGrid.Item>
+                  )}
+                </Form>
+              ) : (
+                <>
+                  <FlexboxGrid.Item colspan={15}>
+                    <h3>{projectData?.name ? projectData.name : 'Project info'}</h3>
+                  </FlexboxGrid.Item>
+                  <FlexboxGrid.Item colspan={5}></FlexboxGrid.Item>
+                  <FlexboxGrid>
+                    <FlexboxGrid.Item>
+                      <IconButton
+                        size="md"
+                        title="Invite users"
+                        icon={<FiUsers />}
+                        onClick={() => {
+                          const link = location.pathname;
+                          navigate(`${link}/user-permissions`);
+                        }}
+                        className={resourceButton}
+                      />
+                    </FlexboxGrid.Item>
+                    <FlexboxGrid.Item>
+                      <ProjectOptions
+                        handleEdit={() => setEditData(true)}
+                        handleDelete={() => setOpenDeleteModal(true)}
+                      />
+                    </FlexboxGrid.Item>
+                  </FlexboxGrid>
+                  <FlexboxGrid.Item colspan={23}>
+                    <Divider style={{ margin: '5px 0' }} />
+                  </FlexboxGrid.Item>
+                  <FlexboxGrid.Item colspan={23} className={descriptionSection}>
+                    <p>
+                      {/* eslint-disable-next-line max-len */}
+                      {projectData?.description
+                        ? projectData.description
+                        : 'No description'}
+                    </p>
+                  </FlexboxGrid.Item>
+
+                  <FlexboxGrid.Item colspan={23} className={descriptionSection}>
+                    <h5>Applications used by the project:</h5>
+                    <AdminDataTable props={tableProps} />
+                  </FlexboxGrid.Item>
+                </>
+              )}
+            </FlexboxGrid>
+
+            {(editData || newProject) && (
+              <FlexboxGrid className={editButtonSection} colspan={20}>
+                <FlexboxGrid.Item colspan={19}></FlexboxGrid.Item>
+                <FlexboxGrid.Item colspan={2}>
+                  <Button
+                    appearance="primary"
+                    onClick={() => updateProject()}
+                    className={editButton}
+                  >
+                    Save
+                  </Button>
+                </FlexboxGrid.Item>
+                {showCancelBtn && (
+                  <FlexboxGrid.Item colspan={3}>
+                    <Button
+                      appearance="subtle"
+                      onClick={() => resetProjectData()}
+                      className={editButton}
+                    >
+                      Cancel
+                    </Button>
+                  </FlexboxGrid.Item>
+                )}
+              </FlexboxGrid>
+            )}
+          </FlexboxGrid.Item>
+          {!editData && !newProject && (
+            <FlexboxGrid.Item
+              colspan={6}
+              className={detailsSection}
+              style={{ backgroundColor: isDark === 'dark' && darkBgColor }}
+            >
+              <FlexboxGrid className={detailsTitle} align="middle">
+                <FlexboxGrid.Item colspan={2} className={detailsTitleIcon}>
+                  <FaCircleInfo />
+                </FlexboxGrid.Item>
+                <FlexboxGrid.Item colspan={15}>
+                  <h3>Project details</h3>
+                </FlexboxGrid.Item>
+              </FlexboxGrid>
+              <Divider style={{ marginTop: '13px' }} />
+              <div
+                className={detailsContent}
+                style={{ backgroundColor: isDark === 'dark' && darkBgColor }}
+              >
+                {projectData?.updated && (
+                  <>
+                    <p className={detailsSubTitle}>Last updated:</p>
+                    <p className={detailsLabel}>{convertTime(projectData.updated)}</p>
+                  </>
+                )}
+                {projectData?.created && (
+                  <>
+                    <p className={detailsSubTitle}>Created:</p>
+                    <p className={detailsLabel}>
+                      Created: {convertTime(projectData.created)}
+                    </p>
+                  </>
+                )}
+                {projectData?.organization?.name && (
+                  <>
+                    <p className={detailsSubTitle}>Organization:</p>
+                    <p className={detailsLabel}>{projectData?.organization?.name}</p>
+                  </>
+                )}
+                {authCtx?.user?.role && (
+                  <>
+                    <p className={detailsSubTitle}>Access level:</p>
+                    <p className={detailsLabel}>{authCtx?.user?.role}</p>
+                  </>
+                )}
+              </div>
+              <Divider style={{ marginTop: '13px' }} />
+              <FlexboxGrid.Item colspan={15} style={{ marginLeft: '15px' }}>
+                <h5>Latest pipeline runs:</h5>
+              </FlexboxGrid.Item>
+              <FlexboxGrid.Item colspan={24} style={{ marginTop: '-15px' }}>
+                {/* eslint-disable-next-line max-len */}
+                <div
+                  style={{ backgroundColor: isDark === 'dark' ? darkBgColor : '#ffffff' }}
+                >
+                  <ProjectPipelines />
+                </div>
+              </FlexboxGrid.Item>
+            </FlexboxGrid.Item>
+          )}
+
+          <AlertModal
+            open={openDeleteModal}
+            setOpen={setOpenDeleteModal}
+            content={DELETION_ALERT_MESSAGE}
+            handleConfirmed={handleDeleteConfirmed}
+          />
+        </>
+      )}
     </FlexboxGrid>
   );
 };
